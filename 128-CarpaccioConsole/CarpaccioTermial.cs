7b0a89528880ed7942abe3f9d7bc52d38838f219
mathieucans <mathieucans@gmail.com>
2016-11-21 20:56:26 +0100

    First track

---
+++ 128-CarpaccioConsole/CarpaccioTermial.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace _128_CarpaccioMsTest
+{
+	public interface IConsole
+	{
+		string ReadLine();
+		void WriteLine(string s);
+	}
+
+	public class CarpaccioTermial
+	{
+		private readonly IConsole _console;
+
+		public CarpaccioTermial(IConsole console)
+		{
+			_console = console;
+		}
+
+		public void DisplayQuantityQuestion()
+		{
+			_console.WriteLine("Entrez une quantité");
+		}
+
+		public void AskAndDisplayQuantity()
+		{
+			string quantity = _console.ReadLine();
+			_console.WriteLine(quantity.Replace(Environment.NewLine,""));
+		}
+	}
+}
---
+++ 128-CarpaccioConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _128_CarpaccioMsTest;
+
+namespace _128_CarpaccioConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var terminal = new CarpaccioTermial(new ConsoleAdapter());
+
+			terminal.DisplayQuantityQuestion();
+			terminal.AskAndDisplayQuantity();
+
+			Console.ReadLine();
+		}
+	}
+
+	internal class ConsoleAdapter : IConsole
+	{
+		public string ReadLine()
+		{
+			return Console.ReadLine();
+		}
+
+		public void WriteLine(string s)
+		{
+			Console.WriteLine(s);
+		}
+	}
+}
