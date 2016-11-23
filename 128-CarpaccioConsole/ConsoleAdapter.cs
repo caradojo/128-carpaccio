a4b51776a0b1764b92eaa6280abefe5417240caa
mathieucans <mathieucans@gmail.com>
2016-11-23 15:01:32 +0100

    aditional files

---
+++ 128-CarpaccioConsole/ConsoleAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using _128_CarpaccioMsTest;
+
+namespace _128_CarpaccioConsole
+{
+	internal class ConsoleAdapter : IConsole
+	{
+		public string ReadLine()
+		{
+			return Console.ReadLine();
+		}
+
+		public void WriteLine(string s)
+		{
+			Console.WriteLine(s);
+		}
+	}
+}
