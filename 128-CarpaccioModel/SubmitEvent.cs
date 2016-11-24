cb53e65e1dd579dee68a497bc09cc8cf46fb9627
mathieucans <mathieucans@gmail.com>
2016-11-24 21:34:56 +0100

    split respnsability SubmitEvent

---
+++ 128-CarpaccioModel/SubmitEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _128_CarpaccioModel
+{
+	public class SubmitEvent : ICarpaccioEventAggregator
+	{
+		public event EventHandler Event;
+
+		public void Publish()
+		{
+			if (Event != null)
+			{
+				Event(this, EventArgs.Empty);
+			}
+		}
+	}
+}
---
+++ 128-CarpaccioMsTest/SubmitEventShould.cs
@@ -0,0 +1,21 @@
+﻿using NFluent;
+using Xunit;
+using _128_CarpaccioModel;
+
+namespace _128_CarpaccioMsTest
+{
+	public class SubmitEventShould
+	{
+		[Fact]
+		public void raise_submit_event_when_publish_submit()
+		{
+			var carpaccioEvetAggregator = new SubmitEvent();
+			int count = 0;
+			carpaccioEvetAggregator.Event += (sender, args) => count++;
+
+			carpaccioEvetAggregator.Publish();
+
+			Check.That(count).Equals(1);
+		}
+	}
+}
