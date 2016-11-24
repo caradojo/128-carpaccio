8215b1c2078af274c07cccfd7d3080ff474e7e03
mathieucans <mathieucans@gmail.com>
2016-11-24 23:06:26 +0100

    split files

---
+++ 128-CarpaccioConsole/ConsoleWriteLineAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _128_CarpaccioConsole
+{
+	internal class ConsoleWriteLineAdapter : IConsoleWriteLine
+	{
+		public void WriteLine(string line)
+		{
+			Console.WriteLine(line);
+		}
+	}
+}
--- 128-CarpaccioConsole/Program.cs
+++ 128-CarpaccioConsole/Program.cs
@@ -17,12 +17,4 @@ namespace _128_CarpaccioConsole
 
 		}
 	}
-
-	internal class ConsoleWriteLineAdapter : IConsoleWriteLine
-	{
-		public void WriteLine(string line)
-		{
-			Console.WriteLine(line);
-		}
-	}
 }
