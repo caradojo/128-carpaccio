cb95b3e042724d0a3e8c4c7b5984043e7cf4a1a3
mathieucans <mathieucans@gmail.com>
2016-12-01 22:26:34 +0100

    remove event concept

---
+++ 128-CarpaccioConsole/OrderService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using _128_CarpaccioModel;
+
+namespace _128_CarpaccioConsole
+{
+	public class OrderService : IOrderService
+	{
+		private readonly IConsoleWriteLine _consoleWriteLine;
+
+		public OrderService(IConsoleWriteLine consoleWriteLine)
+		{
+			_consoleWriteLine = consoleWriteLine;
+		}
+
+		public void SendOrder(IEnumerable<Order> orders)
+		{
+			foreach (var order in orders)
+			{
+				_consoleWriteLine.WriteLine(string.Format("Total  {0:F}", order.Amout));
+			}
+		}
+	}
+}
---
+++ CarpaccioConsoleTest/OrderServiceShould.cs
@@ -0,0 +1,21 @@
+﻿using FakeItEasy;
+using Xunit;
+using _128_CarpaccioConsole;
+using _128_CarpaccioModel;
+
+namespace CarpaccioConsoleTest
+{
+	public class OrderServiceShould
+	{
+		[Fact]
+		public void write_total_order_in_console()
+		{
+			var consoleWriteLine = A.Fake<IConsoleWriteLine>();
+			var orderService = new OrderService(consoleWriteLine);
+
+			orderService.SendOrder(new [] {new Order(125.151)});
+
+			A.CallTo(() => consoleWriteLine.WriteLine("Total: 125.15"));
+		}
+	}
+}
