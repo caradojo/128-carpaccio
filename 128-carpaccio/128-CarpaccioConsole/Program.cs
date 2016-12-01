fd8e35ecc0640e95b1165c52e5a9f062083c4e2b
mathieucans <mathieucans@gmail.com>
2016-12-01 22:10:12 +0100

    oups

---
+++ 128-carpaccio/128-CarpaccioConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using _128_CarpaccioMsTest;
+
+namespace _128_CarpaccioConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = Encoding.UTF8;
+
+			var terminal = new CarpaccioTermial(new ConsoleAdapter());
+
+			terminal.DisplayQuantityQuestion();
+			terminal.AskAndDisplayQuantity();
+			terminal.DisplayPriceQuestion();
+			terminal.AskAndDisplayPrice();
+
+			terminal.DisplayTotal(22);
+
+			Console.WriteLine("Appuyer sur une touche pour terminer");
+			Console.ReadLine();
+
+		}
+	}
+}
