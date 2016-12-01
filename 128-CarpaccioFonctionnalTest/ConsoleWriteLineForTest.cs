0c213e9ad69aaa02a01f6ac178b687ceac44d481
mathieucans <mathieucans@gmail.com>
2016-12-01 22:32:42 +0100

    split class in files

---
+++ 128-CarpaccioFonctionnalTest/ConsoleWriteLineForTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using _128_CarpaccioConsole;
+
+namespace _128_CarpaccioFonctionnalTest
+{
+	public class ConsoleWriteLineForTest : IConsoleWriteLine
+	{
+		private readonly StringBuilder _output;
+
+		public ConsoleWriteLineForTest()
+		{
+			_output = new StringBuilder();
+		}
+
+		public string Output
+		{
+			get { return _output.ToString(); }
+		}
+
+		public void WriteLine(string line)
+		{
+			_output.AppendLine(line);
+		}
+	}
+}
